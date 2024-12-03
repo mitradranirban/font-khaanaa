40f2c8559e10d059a980ab92d3d4aee254212ffd
Dr Anirban Mitra <mitradranirban@gmail.com>
2024-12-03 09:33:12 +0530

    202412030930

--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 rm -rf fonts
 rm -rf build
 source venv/bin/activate
-fonts2svg -c 000000,00ff00,ffff00,ff0000  sources/ttf/Khaanaa-Black.ttf sources/ttf/Khaanaa-green.ttf \
+fonts2svg -c 000000,00ff00,e3ab45,ff0000  sources/ttf/Khaanaa-Black.ttf sources/ttf/Khaanaa-green.ttf \
 sources/ttf/Khaanaa-yellow.ttf sources/ttf/Khaanaa-red.ttf
 addsvg sources/ttf/SVGs/ sources/ttf/Khaanaa.ttf
-maximum_color sources/ttf/Khaanaa.ttf
-mkdir -p fonts/otsvg fonts/ttf fonts/woff2
+maximum_color --bitmaps sources/ttf/Khaanaa.ttf
+mkdir -p fonts/otsvg fonts/ttf fonts/woff2 fonts/cbdt
 cp sources/ttf/Khaanaa.ttf fonts/otsvg/
 ttx -x SVG   -o KhaanaaColorEmoji.ttx -v build/Khaanaa.keep_glyph_names.added_glyf_colr_1.ttf
+ttx -x SVG -o KhaanaaCBDT.ttx -v build/Khaanaa.added_cbdt.ttf
 ttx -d fonts/ttf/ KhaanaaColorEmoji.ttx
 ttx -d fonts/woff2/ --flavor woff2 KhaanaaColorEmoji.ttx
+ttx -d fonts/cbdt/ KhaanaaCBDT.ttx
 mv KhaanaaColorEmoji.ttx sources/ttx/
+mv KhaanaaCBDT.ttx sources/ttx/
 
--- documentation/codepoints.md
+++ documentation/codepoints.md
@@ -4,7 +4,8 @@ Following emojis are encoded in Unicode Private Use Area in the font
 
 |Sl No|shortcode|unicode hex code|picture|
 |:-----|:---------:|:----------------:|-------|
-|1 |namaste| u1F89F|![namaste](../sources/ttf/SVGs/u1F89F.svg)|
-|2 |jalebi|  uniE366|![jalebi](../sources/ttf/SVGs/uniE366.svg)|
-|3|samosa|   uniE367|![samosa](../sources/ttf/SVGs/uniE367.svg)|
+|1 |namaste| &#1F64F|![namaste](../sources/ttf/SVGs/u1F64F.svg)|
+|2 |jalebi| &#xE366|![jalebi](../sources/ttf/SVGs/uniE366.svg)|
+|3|samosa|   &#E367|![samosa](../sources/ttf/SVGs/uniE367.svg)|
+|4|laddoo| &#E368|![samosa](../sources/ttf/SVGs/uniE368.svg)|
 
