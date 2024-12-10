8584ee952a88c8e446e4ae9699ff0ab6375765cd
Dr Anirban Mitra <mitradranirban@gmail.com>
2024-12-10 12:46:47 +0530

    removed binary files from git sources

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
+rm -rf sources/ttf
+mkdir sources/ttf
 cd sources/sfd
 ./generate.pe *.sfd
 cd ../..
 mv sources/sfd/*.ttf sources/ttf/
 rm -rf fonts
 rm -rf build
+
 source venv/bin/activate
 fonts2svg -c 000000,00ff00,e3ab45,ff0000  sources/ttf/Khaanaa-black.ttf sources/ttf/Khaanaa-green.ttf \
 sources/ttf/Khaanaa-yellow.ttf sources/ttf/Khaanaa-red.ttf
@@ -13,7 +15,7 @@ addsvg sources/ttf/SVGs/ sources/ttf/Khaanaa.ttf
 maximum_color --bitmaps sources/ttf/Khaanaa.ttf
 mkdir -p fonts/otsvg fonts/ttf fonts/woff2 fonts/cbdt
 cp sources/ttf/Khaanaa.ttf fonts/otsvg/
-ttx -x SVG -x FFTM  -o KhaanaaColorEmoji.ttx -v build/Khaanaa.keep_glyph_names.added_glyf_colr_1.ttf
+ttx -x FFTM  -o KhaanaaColorEmoji.ttx -v build/Khaanaa.keep_glyph_names.added_glyf_colr_1.ttf
 ttx -x SVG -x FFTM -o KhaanaaCBDT.ttx -v build/Khaanaa.added_cbdt.ttf
 ttx -d fonts/ttf/ KhaanaaColorEmoji.ttx
 ttx -d fonts/woff2/ --flavor woff2 KhaanaaColorEmoji.ttx
